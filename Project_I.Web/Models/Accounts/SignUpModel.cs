618d017478c4af42baa42bf8064dc333a27f739c
abhijeet9238 <63091634+abhijeet9238@users.noreply.github.com>
2020-06-30 00:03:57 +0530

    Intial staging

---
+++ Project_I.Web/Models/Accounts/SignUpModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace Project_I.Web.Models.Accounts
+{
+    public class SignUpModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name ="Email")]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(6,ErrorMessage ="Password required  6 char long")]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(6, ErrorMessage = "Password required  6 char long")]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password",ErrorMessage ="Don't match")]
+        public string ConfirmPassowrd { get; set; }
+    }
+}
